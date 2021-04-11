8254c9019088bd93b9e83d202253ee5c49b331dc
boochang <boomboom0623@gmail.com>
2021-04-11 17:09:06 +0900

    Test push (#2)
    * test commit
    
    * test second

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <h1 className={styles.title}>
           fuga Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
+        <h2>hoge</h2>
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
